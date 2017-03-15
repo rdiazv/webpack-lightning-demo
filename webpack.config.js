8c49f668ffd34eff4db8a8a5732959da4a505ca8
Rodrigo Andrés Díaz Vergara <rdiaz@lemontech.cl>
2017-03-15 13:01:15 -0300

    Dev server.
    Adds the `dev` script to run `webpack-dev-server` on port 8080.
    Uses `html-webpack-plugin` to auto generate an `index.html` based a template and the webpack configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: path.resolve('src/entries/app.js'),
@@ -6,4 +7,9 @@ module.exports = {
     path: path.resolve('build'),
     filename: 'app.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('src/index.html'),
+    })
+  ],
 }
